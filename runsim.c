e0ef21e380e85f553cf63070c14ae038bf84bd27
Matt Bernardini <matt.bernardini@gmail.com>
2017-12-04 21:30:43 -0600

    found some stuff on stack overflow, started modifiying it to fit what i need it to do

--- runsim.c
+++ runsim.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+int pr_count;
 int main (int argc, char *argv[]) {
-  pid_t childpid = 0;
-  int i, n;
+  int pr_limit;
 
   if (argc != 2){ /* check for valid number of command-line arguments */
     fprintf(stderr, "Usage: %s processes\n", argv[0]);
     return 1;
   }
 
-  n = atoi(argv[1]);
-  for (i = 1; i < n; i++)
-    if ((childpid = fork()) <= 0)
-      break;
+  pr_limit = atoi(argv[1]);
+  for (int i = 1; i < n; i++)
+    int childpid = fork()
+    switch (childpid)
+    {
+      case -1:
+      {
+          printf("Error\n");
+          exit(255);
+      }
+      case 0:
+      {
+        exec()
+
+        children = children - 1;
+        if (children < 0) children = 0;
+
+        exit(1);
+      }
+      default:
+      {   
+        if (children > pr_limit)
+            wait();
+        children++;
+
+        pid = fork();
+      }
 
   fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
                         i, getpid(), getppid(), childpid);
