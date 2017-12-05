7debd899488f007d2426bc97dc9f2ed3079f25e5
Matt Bernardini <matt.bernardini@gmail.com>
2017-12-05 17:44:59 -0600

    started implementing shared memory to store the number of running processes

--- runsim.c
+++ runsim.c
@@ -3,14 +3,25 @@
 #include <unistd.h>
 int pr_count;
 int main (int argc, char *argv[]) {
-  int pr_limit;
-
-  if (argc != 2){ /* check for valid number of command-line arguments */
+  // Check for valid number of command-line arguments
+  if (argc != 2){ 
     fprintf(stderr, "Usage: %s processes\n", argv[0]);
     return 1;
   }
 
+  // Declare variables to be used in the rest of the program
+  pid_t cpid;
+  int shmId,i,pr_limit,*shmPtr;
+  key_t key;
+
+  // Allocate shared memory to store the number of running processes
+  (shmId = shmget(key, sizeof(int), IPC_CREAT | 0666));
+  shmPtr = (int *) shmat( shmId, NULL, 0);
+
+  // Set the number of concurrent processes to let run
   pr_limit = atoi(argv[1]);
+
+
   for (int i = 1; i < n; i++)
     int childpid = fork()
     switch (childpid)
@@ -43,3 +54,34 @@ int main (int argc, char *argv[]) {
                         
   return 0;
 }
+/*
+#define _XOPEN_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include<sys/wait.h>
+
+int main() {
+  pid_t cpid;
+  int shmId,i;
+  key_t key;
+  int *shmPtr;
+  const int SIZE = 10;
+  (shmId = shmget(key, sizeof(int), IPC_CREAT | 0666));
+  shmPtr = (int *) shmat( shmId, NULL, 0);
+  *shmPtr = 10;
+  int pid = fork();
+  if (pid == 0) {
+    *shmPtr = 1;
+    exit(0);
+  }
+  cpid= wait(NULL);
+  printf("%d\n", *shmPtr);
+  shmdt(shmPtr);
+  shmctl(shmId,IPC_RMID,NULL);
+  return 0;
+}
+*/
