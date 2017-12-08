42bbd148d62279b170b032acc3e96f0aac59af18
Matt Bernardini <matt.bernardini@gmail.com>
2017-12-08 13:59:31 -0600

    put in wrong spot

--- runsim.c
+++ runsim.c
@@ -12,31 +12,27 @@
 #include <string.h>
 #include <stddef.h>
 int main (int argc, char *argv[]) {
-
   // Check for valid number of command-line arguments
   if (argc != 2){ 
     fprintf(stderr, "Usage: %s processes\n", argv[0]);
     return 1;
   }
-
   // Declare variables to be used in the rest of the program
   pid_t cpid;
   int shmId,i,pr_limit,*pr_current, childpid, status;
   key_t key;
   char command[MAX_COMMAND_SIZE];
-
   // Allocate shared memory to store the number of running processes
   shmId = shmget(key, sizeof(int), IPC_CREAT | 0666);
   pr_current = (int *) shmat( shmId, NULL, 0);
   *pr_current = 0;
-  char *commandList = strsep(command, " ");
-  printf("%s\n", commandList);
   // Set the number of concurrent processes to let run
   pr_limit = atoi(argv[1]);
 
   while (fgets(command, MAX_COMMAND_SIZE, stdin) != NULL) {
     // Handle error
-    
+    char *commandList = strsep(command, " ");
+    printf("%s\n", commandList);
     childpid = fork();
     if (childpid != 0) {
       *pr_current += 1;
