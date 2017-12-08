d411a7309721282d37fe35532ff83661e2a3af24
Matt Bernardini <matt.bernardini@gmail.com>
2017-12-08 13:42:25 -0600

    logic changes

--- runsim.c
+++ runsim.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -19,7 +20,7 @@ int main (int argc, char *argv[]) {
 
   // Declare variables to be used in the rest of the program
   pid_t cpid;
-  int shmId,i,pr_limit,*pr_current, childpid;
+  int shmId,i,pr_limit,*pr_current, childpid, status;
   key_t key;
   char command[MAX_COMMAND_SIZE];
 
@@ -34,16 +35,20 @@ int main (int argc, char *argv[]) {
   while (fgets(command, MAX_COMMAND_SIZE, stdin) != NULL) {
     // Handle error
     
-    childpid = fork();
+    childpid = vfork();
     if (childpid != 0) {
       *pr_current += 1;
       // Parent
+      if (*pr_current > 2) {
+        childpid = wait(&status);
+      }
     } else {
       // Child process
 
 
       printf("%d\n", *pr_current);
       execl("./testsim", "./testsim", "3", "1", (char*) NULL);
+      exit(1)
 
     }
     if (*pr_current >= pr_limit & childpid != 0) {
